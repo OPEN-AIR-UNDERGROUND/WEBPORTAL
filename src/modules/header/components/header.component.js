2c26cc28210448ee9476d7c5764b8994ad4d2757
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-04-07 21:17:14 +0200

    feat: added Header component

---
+++ src/modules/header/components/header.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header, Image, Button } from 'semantic-ui-react'
+ 
+const HeaderOpenAir = () => (
+  <Header as='h2' color='black' dividing='true' textAlign='left' block>
+     &nbsp;&nbsp;  
+    <Image circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />  Open Air Festival &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <Button icon='ticket' content='Tickets' color='black'></Button>
+  </Header>
+)
+
+export default HeaderOpenAir;
