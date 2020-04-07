918f06fe768469cbbc4fb3c25ff1cab922bb185c
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-04-07 23:02:48 +0200

    feat: added Menu component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router,Route,Switch,Redirect } from 'react-router-dom';
 import HeaderOpenAir from './modules/header/components/header.component';
-
+import Menu from './modules/menu/components/menu.component';
 
 const  App = () =>  {
   return (
     <div className="App">
      <Router>
        <HeaderOpenAir></HeaderOpenAir>
+       <Menu></Menu>
        <Switch>
 
        </Switch>
--- src/modules/header/components/header.component.js
+++ src/modules/header/components/header.component.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Header, Image, Button } from 'semantic-ui-react'
- 
+
 const HeaderOpenAir = () => (
-  <Header as='h2' color='black' dividing='true' textAlign='left' block>
+  <div className='item-header'>
+    <Header as='h2' color='black' dividing='true' textAlign='left' block>
      &nbsp;&nbsp;  
     <Image circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />  Open Air Festival &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     <Button icon='ticket' content='Tickets' color='black'></Button>
   </Header>
+  </div>  
+ 
 )
 
 export default HeaderOpenAir;
