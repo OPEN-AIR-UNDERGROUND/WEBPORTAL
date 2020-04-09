40fc72c3138a6fd49cc8fd5b8a275be09aac8c36
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-04-10 00:11:30 +0200

    feat: added Grid in artist.component

--- src/modules/artist/components/artists.component.js
+++ src/modules/artist/components/artists.component.js
@@ -1,8 +1,59 @@
 import React from 'react'
+import { Grid, Image, Container } from 'semantic-ui-react'
 
 const Artists = () => {
     return(
-        <p>Artists</p>
+        <Container>
+        <Grid columns={3} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit non, hendrerit venenatis dignissim mus cursus congue rutrum lectus, ultrices odio a praesent vulputate torquent proin. Curae aenean ac tortor vehicula, nostra sem luctus phasellus aliquam, accumsan est quam. Sed habitant cras ad nascetur nibh maecenas libero, scelerisque risus himenaeos pulvinar massa nulla, gravida purus consequat quam tempus nullam.
+            Litora lacus mauris magna dignissim donec curabitur senectus hac tincidunt fermentum, sagittis semper conubia tristique sollicitudin praesent ante class duis, metus cras dui at arcu consequat vehicula enim lacinia. Ligula dis viverra facilisis purus pulvinar massa velit vestibulum, porta mauris vel ullamcorper dignissim mattis sed lectus sociis, suscipit malesuada nisl vulputate curae ridiculus leo. Est morbi tristique eleifend dictum euismod vitae placerat a, turpis magnis vehicula nec sociosqu leo inceptos vestibulum, neque integer congue odio faucibus aptent pharetra.
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit non, hendrerit venenatis dignissim mus cursus congue rutrum lectus, ultrices odio a praesent vulputate torquent proin. Curae aenean ac tortor vehicula, nostra sem luctus phasellus aliquam, accumsan est quam. Sed habitant cras ad nascetur nibh maecenas libero, scelerisque risus himenaeos pulvinar massa nulla, gravida purus consequat quam tempus nullam.
+            Litora lacus mauris magna dignissim donec curabitur senectus hac tincidunt fermentum, sagittis semper conubia tristique sollicitudin praesent ante class duis, metus cras dui at arcu consequat vehicula enim lacinia. Ligula dis viverra facilisis purus pulvinar massa velit vestibulum, porta mauris vel ullamcorper dignissim mattis sed lectus sociis, suscipit malesuada nisl vulputate curae ridiculus leo. Est morbi tristique eleifend dictum euismod vitae placerat a, turpis magnis vehicula nec sociosqu leo inceptos vestibulum, neque integer congue odio faucibus aptent pharetra.   
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit non, hendrerit venenatis dignissim mus cursus congue rutrum lectus, ultrices odio a praesent vulputate torquent proin. Curae aenean ac tortor vehicula, nostra sem luctus phasellus aliquam, accumsan est quam. Sed habitant cras ad nascetur nibh maecenas libero, scelerisque risus himenaeos pulvinar massa nulla, gravida purus consequat quam tempus nullam.
+            Litora lacus mauris magna dignissim donec curabitur senectus hac tincidunt fermentum, sagittis semper conubia tristique sollicitudin praesent ante class duis, metus cras dui at arcu consequat vehicula enim lacinia. Ligula dis viverra facilisis purus pulvinar massa velit vestibulum, porta mauris vel ullamcorper dignissim mattis sed lectus sociis, suscipit malesuada nisl vulputate curae ridiculus leo. Est morbi tristique eleifend dictum euismod vitae placerat a, turpis magnis vehicula nec sociosqu leo inceptos vestibulum, neque integer congue odio faucibus aptent pharetra.   
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+    
+        <Grid.Row>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit non, hendrerit venenatis dignissim mus cursus congue rutrum lectus, ultrices odio a praesent vulputate torquent proin. Curae aenean ac tortor vehicula, nostra sem luctus phasellus aliquam, accumsan est quam. Sed habitant cras ad nascetur nibh maecenas libero, scelerisque risus himenaeos pulvinar massa nulla, gravida purus consequat quam tempus nullam.
+            Litora lacus mauris magna dignissim donec curabitur senectus hac tincidunt fermentum, sagittis semper conubia tristique sollicitudin praesent ante class duis, metus cras dui at arcu consequat vehicula enim lacinia. Ligula dis viverra facilisis purus pulvinar massa velit vestibulum, porta mauris vel ullamcorper dignissim mattis sed lectus sociis, suscipit malesuada nisl vulputate curae ridiculus leo. Est morbi tristique eleifend dictum euismod vitae placerat a, turpis magnis vehicula nec sociosqu leo inceptos vestibulum, neque integer congue odio faucibus aptent pharetra.   
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit non, hendrerit venenatis dignissim mus cursus congue rutrum lectus, ultrices odio a praesent vulputate torquent proin. Curae aenean ac tortor vehicula, nostra sem luctus phasellus aliquam, accumsan est quam. Sed habitant cras ad nascetur nibh maecenas libero, scelerisque risus himenaeos pulvinar massa nulla, gravida purus consequat quam tempus nullam.
+            Litora lacus mauris magna dignissim donec curabitur senectus hac tincidunt fermentum, sagittis semper conubia tristique sollicitudin praesent ante class duis, metus cras dui at arcu consequat vehicula enim lacinia. Ligula dis viverra facilisis purus pulvinar massa velit vestibulum, porta mauris vel ullamcorper dignissim mattis sed lectus sociis, suscipit malesuada nisl vulputate curae ridiculus leo. Est morbi tristique eleifend dictum euismod vitae placerat a, turpis magnis vehicula nec sociosqu leo inceptos vestibulum, neque integer congue odio faucibus aptent pharetra.   
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit non, hendrerit venenatis dignissim mus cursus congue rutrum lectus, ultrices odio a praesent vulputate torquent proin. Curae aenean ac tortor vehicula, nostra sem luctus phasellus aliquam, accumsan est quam. Sed habitant cras ad nascetur nibh maecenas libero, scelerisque risus himenaeos pulvinar massa nulla, gravida purus consequat quam tempus nullam.
+            Litora lacus mauris magna dignissim donec curabitur senectus hac tincidunt fermentum, sagittis semper conubia tristique sollicitudin praesent ante class duis, metus cras dui at arcu consequat vehicula enim lacinia. Ligula dis viverra facilisis purus pulvinar massa velit vestibulum, porta mauris vel ullamcorper dignissim mattis sed lectus sociis, suscipit malesuada nisl vulputate curae ridiculus leo. Est morbi tristique eleifend dictum euismod vitae placerat a, turpis magnis vehicula nec sociosqu leo inceptos vestibulum, neque integer congue odio faucibus aptent pharetra.   
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      </Container>  
     )
 }
 
