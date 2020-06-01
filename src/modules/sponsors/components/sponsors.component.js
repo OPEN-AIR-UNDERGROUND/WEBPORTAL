6ddf151fb01d53f39a0b65da4cf6e269a4f4468d
Bladi <bladimaltego@gmail.com>
2020-06-01 19:06:46 +0200

    feat:added Sponsors grid

--- src/modules/sponsors/components/sponsors.component.js
+++ src/modules/sponsors/components/sponsors.component.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const Sponsors = () => {
-    return(
-        <p>Sponsors</p>
-    )
-}
+import { Grid, Image } from 'semantic-ui-react'
+
+const Sponsors = () => (
+  <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
 
 export default Sponsors;
